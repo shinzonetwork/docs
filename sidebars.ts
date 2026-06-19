6526a57d5fe8312f252e385565bd679f01704a96
Johnny <9611008+johnnymatthews@users.noreply.github.com>
2026-06-19 14:06:48 +0100

    Includes file moves in sidebar.

--- sidebars.ts
+++ sidebars.ts
@@ -52,7 +52,8 @@ const sidebars: SidebarsConfig = {
       type: "category",
       label: "Guides",
       items: [
-        "guides/building-apps-with-shinzo"
+        "guides/building-apps-with-shinzo/index",
+        "guides/operator-quickstart/index"
       ],
     },
     {
